291366226868c541e80bd05778a5eb947fea962d
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-06-12 15:01:53 -0700

    updated to meet MVP

--- assignments/this.js
+++ assignments/this.js
@@ -25,18 +25,52 @@ const patient = {
   patientSummary() {
     return `This patient ${this.name} with a date of birth ${
       this.DOB
-    } and lives on ${this.address} does not have a diagnosis of ${this.Dx}`;
+    } and lives on ${this.Address} does not have a diagnosis of ${this.Dx}.`;
   }
 };
 
-// Principle 3
-// function house(stories, model, year) {
-//   this.stories = 2;
-//   this.model = whatever a house model is;
-//   this.year = 2019;
-// }
-// console.log(house);
+console.log(patient.patientSummary());
 
+// Principle 3
+
+function myfamily(neice, nephew) {
+  this.neice = neice;
+  this.nephew = nephew;
+  this.summary = function() {
+    console.log(`My Neice is ${neice}. My nephew is ${nephew}.`);
+  };
+}
+const family1 = new myfamily("Ruby Genevieve", "Russell James");
+const family2 = new myfamily("Jessie Ashlee", "Josh Evan");
+
+family1.summary();
+family2.summary();
 // Principle 4
 
 // code example for Explicit Binding
+function Patient(attritbutes) {
+  this.name = attritbutes.name;
+  this.dob = attritbutes.dob;
+  this.dx = attritbutes.dx;
+
+  this.charts = function() {
+    return `During today's appointment Patient ${
+      this.name
+    } with a Date Of Birth of ${this.dob} was diagnosed with ${this.dx}.`;
+  };
+}
+
+const jennifer = new Patient({
+  name: "Jennifer Diamond",
+  dob: "03/05/1969",
+  dx: "F90.9 - Attention-deficit hyperactivity disorder, unspecified type"
+});
+
+const todd = new Patient({
+  name: "Todd Hanle",
+  dob: "04/23/1967",
+  dx: "I10 - Essential (primary) hypertension"
+});
+
+console.log(jennifer.charts());
+console.log(jennifer.charts.call(todd));
