84700bfabcf1fa8443cf089b0bc6ed92b266e890
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-06-12 00:06:09 -0700

    started to work on the project

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Global/window binding- bind() makes a function that is called with a particular this value.
+ * 2. Implicit binding occurs when dot notation is used to invoke a function.
+ * 3. New Binding creates a blank, plain JavaScript object and Returns this if the function doesn't return.
+ * 4. Explicit binding of this occurs when .call(), .apply(), or .bind() are used on a function.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
@@ -18,8 +18,12 @@
 // code example for Implicit Binding
 
 // Principle 3
-
-// code example for New Binding
+function House(stories, model, year) {
+  this.stories = stories;
+  this.model = model;
+  this.year = year;
+}
+console.log(House);
 
 // Principle 4
 
